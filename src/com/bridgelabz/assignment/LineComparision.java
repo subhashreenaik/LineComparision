57fb47059cc2858c330b9a137da07a27f92066a4
Subhashree naik <subhashree.naik@gmail.com>
2022-02-14 20:24:22 +0530

    welcome message

---
+++ src/com/bridgelabz/assignment/LineComparision.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz.assignment;
+
+/**
+ * @author subhashree
+ *
+ */
+public class LineComparision {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to Line\r\n"
+       		+ "Comparison Computation\r\n"
+       		+ "Program ");
+	}
+
+}

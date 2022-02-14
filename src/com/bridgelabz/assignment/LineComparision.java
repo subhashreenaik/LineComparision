7d4162443d6bc3d6c89fdec6a036d76ea7a6d893
Subhashree naik <subhashree.naik@gmail.com>
2022-02-14 20:45:22 +0530

    calculation of first Line

--- src/com/bridgelabz/assignment/LineComparision.java
+++ src/com/bridgelabz/assignment/LineComparision.java
@@ -3,18 +3,37 @@
  */
 package com.bridgelabz.assignment;
 
+import java.util.Scanner;
+
 /**
  * @author subhashree
  *
  */
 public class LineComparision {
-
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("Welcome to Line\r\n"
-       		+ "Comparison Computation\r\n"
-       		+ "Program ");
-	}
+	//This method computes the length of the line
+	static void calculateLengthLine(double x1,double x2,double y1,double y2) {
+ 	 double length_of_line=Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+	 System.out.println("Length of the line is  :"+length_of_line);
+ 	 
+  }
+	
+  public static void main(String[] args) {
+	 
+	   //Taking inputs from system
+		Scanner scan= new Scanner(System.in);
+	    System.out.println("enter points of first line and second line");
+	       
+	       double x1=scan.nextDouble();
+	       double y1=scan.nextDouble();
+	       double x2=scan.nextDouble();
+	       double y2=scan.nextDouble();
+	       
+	       //calling the method
+	       calculateLengthLine(x1,x2,y1,y2);
+	       
+	    scan.close();
+	       
+ }
 
 }
